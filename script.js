165d09f3e6cf7aaff8b3b60852f496526f4d4b6f
Law-catz2 <Lawrence.cataylo@foundationu.com>
2026-05-26 16:22:08 +0800

    styling part 1

--- script.js
+++ script.js
@@ -33,13 +33,14 @@ for(let i = 0; i < whoseBlog.length; ++i) {
 /* This is for the home page*/
 function whoAmI() {
     let p = document.querySelector('p')
-    p.textContent ="Lawrence G. Cataylo"
+    p.innerHTML ="<p style='text-align: center;'>Lawrence G. Cataylo</p>"
 }
 
 
 function displaySecret() {
-     p = document.querySelector('p')
-    p.innerHTML ="<h2>Hey everyone! I'm Lawrence.</h2>I'm currently studying Computer Science at Foundation University. I program as a hobby. Yet, I explore a wide range of skills as a hobbyist. I gain enough knowledge to challenge the rules of society. I'm ready to learn and work along with you.<br><br>Thanks!"
+    p = document.querySelector('p')
+    
+    p.innerHTML ="<img class='profile-pic' src = 'lgc.jpg' width = 300><h2>Hey everyone! I'm Lawrence.</h2>I'm currently studying Computer Science at Foundation University. I program as a hobby. Yet, I explore a wide range of skills as a hobbyist. I gain enough knowledge to challenge the rules of society. I'm ready to learn and work along with you.<br><br>Thanks!"
 }
 
 
